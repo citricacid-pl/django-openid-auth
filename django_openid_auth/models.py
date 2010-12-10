cffdcfe339867153052b6ecb8f401de6e7e8da19
Jannis Leidel <jannis@leidel.info>
2010-12-10 15:07:03 +0100

    Fixed TextFields in Association and UserOpenID models to not contain a max_length.

--- django_openid_auth/models.py
+++ django_openid_auth/models.py
@@ -41,12 +41,12 @@ class Nonce(models.Model):
 
 
 class Association(models.Model):
-    server_url = models.TextField(max_length=2047)
+    server_url = models.TextField()
     handle = models.CharField(max_length=255)
-    secret = models.TextField(max_length=255) # Stored base64 encoded
+    secret = models.TextField()
     issued = models.IntegerField()
     lifetime = models.IntegerField()
-    assoc_type = models.TextField(max_length=64)
+    assoc_type = models.TextField()
 
     def __unicode__(self):
         return u"Association: %s, %s" % (self.server_url, self.handle)
@@ -54,5 +54,5 @@ class Association(models.Model):
 
 class UserOpenID(models.Model):
     user = models.ForeignKey(User)
-    claimed_id = models.TextField(max_length=2047, unique=True)
-    display_id = models.TextField(max_length=2047)
+    claimed_id = models.TextField()
+    display_id = models.TextField()
